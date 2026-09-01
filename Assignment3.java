a44410cd766b6d17931c00391ae7081687232d3e
godfreyrampel <godfreyramospelingen@gmail.com>
2026-09-01 17:16:41 +0800

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,30 @@
+public class Assignment {
+    public static void main(String[] args) {
+
+        // Stores the first name as text
+        String Firstname = "Godfrey";
+        
+        // Stores the last name as text
+        String Lastname = "Pelingen";
+
+        // Stores the address as text
+        String Address = "Manaol, Poz. Pang.";
+
+        // Stores the mobile number as text
+        String MobileNumber = "09162737667";
+
+        // Stores the age as a whole number
+        int Age = 18;
+
+        // Stores the GWA as a decimal number
+        double GWA = 93.0;
+
+        // Displays the information
+        System.out.println("Firstname: " + Firstname);
+        System.out.println("Lastname: " + Lastname);
+        System.out.println("Address: " + Address);
+        System.out.println("Mobile Number: " + MobileNumber);
+        System.out.println("Age: " + Age);
+        System.out.println("GWA: " + GWA);
+    }
+}
