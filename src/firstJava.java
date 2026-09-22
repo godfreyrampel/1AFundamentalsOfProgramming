c131414e56512654f1601bfa035ce80ef52b9219
godfreyrampel <godfreyramospelingen@gmail.com>
2026-09-22 21:11:30 +0800

    Initial commit

---
+++ src/firstJava.java
@@ -0,0 +1,6 @@
+public class firstJava {
+    public  static void main(String[] args) {
+        System.out.println("Godfrey Ramos Pelingen");
+        System.out.print("BSIT-1A");
+    }
+}
---
+++ src/secondJava.java
@@ -0,0 +1,15 @@
+public class secondJava {
+    public static void main(String[] args){
+        String name = "Godfrey Ramos Pelingen";
+        String address = "Manaol,Poz,Pang";
+        String contact = "09162737667";
+        int age =  18;
+        String crush = "Lhance";
+
+        System.out.println("name:" + name);
+        System.out.println("address" + address);
+        System.out.println("contact:" + contact);
+        System.out.println("age:" + age);
+        System.out.println("crush:" + crush);
+    }
+}
